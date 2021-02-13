ec5e8fc61230a524888702f11af4cabc25977945
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-02-14 00:43:57 +0500

    this is the practice file of javascript.

---
+++ arithmetic.js
@@ -0,0 +1,49 @@
+
+document.write("<h1>ARITHMETIC OPERATORS.</h1>")
+
+document.write("ADDITION" + "<br><br>");
+
+// Addition 
+
+var a = 30;
+var b = 50;
+c = a + b;
+document.write(c + "<br><br>");
+
+document.write("SUBSTRACTION" + "<br><br>");
+
+// Substraction
+
+var a1 = 50;
+var b1 = 70;
+c1 = a1 - b1;
+document.write(c1 + "<br><br>" );
+
+document.write("MULTIPLICATION" + "<br><br>");
+
+// Multiplication
+
+var a2 = 50;
+var b2 = 70;
+c2 = a2 * b2;
+document.write(c2 + "<br><br>");
+
+document.write("Division" + "<br><br>");
+
+// Division
+
+var a3 = 50;
+var b3 = 70;
+c3 = a3 / b3;
+document.write(c3 + "<br><br>");
+
+
+document.write("Modulus" + "<br><br>");
+
+// Modulus
+
+var a4 = 50;
+var b4 = 70;
+c4 = a4 % b4;
+document.write(c4 + "<br><br>");
+
