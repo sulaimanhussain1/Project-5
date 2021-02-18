d4d00718941f8a57def4aa8eaadfb600cbab590b
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-02-18 18:38:16 +0500

    This is the practice of javascript.

---
+++ assignment.js
@@ -0,0 +1,27 @@
+
+
+document.write("<h1>Assignment Operators.</h1>");
+ 
+
+ var a = 30;
+ a += 4 ;
+ document.write(a);
+document.write("<br>");
+
+
+var a = 30;
+a -= 4 ;
+document.write(a);
+document.write("<br>");
+
+
+var a = 30;
+a *= 4 ;
+document.write(a);
+document.write("<br>");
+
+
+var a = 30;
+a /= 4 ;
+document.write(a);
+document.write("<br>");
