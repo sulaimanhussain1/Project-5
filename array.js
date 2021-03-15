c6ca14c3561046f7c3b5ff63c9f466511cdae63e
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-03-15 18:13:55 +0500

    This is the practice of javascript

---
+++ array.js
@@ -0,0 +1,11 @@
+
+
+var foods = ["Burger" , "Pizza" , "Sandwich" , "Chips"];
+
+alert("FOODS NAME!");
+alert(foods[0]);
+alert( foods[1]);
+alert(foods[2]);
+alert(foods[3]);
+
+console.log(foods);
