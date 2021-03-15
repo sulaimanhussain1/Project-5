2169969ea54489638ed4d5fe5ea8265ca61448ae
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-03-16 02:51:11 +0500

    This is the practice file of javascript

---
+++ BODMAS.js
@@ -0,0 +1,27 @@
+
+
+document.write("<h1>BODMAS...</h1>" + "<br><br>") ;
+
+var a= 10 + 23 - 3 / 3;   //   32 
+var b = 5 / 5 - 3 ;     // -2
+var c = a + 32 - 21 * (3 + b) / 6 ;     // 60.5
+
+// B = bracket first
+// 0 = orders 
+// D = Division
+// M = Multiplication
+// A = Addition
+// S = Substraction
+
+console.log (a + "<br>");
+console.log(b + "<br>");
+console.log(c + "<br>");
+
+document.write("<h1>Increment and Decrement...</h1>");
+
+var a1 = 7;
+var b1 = ++a1;
+
+console.log(a1);
+console.log(b1);
+
