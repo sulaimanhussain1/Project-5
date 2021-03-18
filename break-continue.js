ee65d8567f1c28b2b281b8da78d0a7c0ec7040a3
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-03-18 21:36:24 +0500

    This is the practice of javascript

---
+++ break-continue.js
@@ -0,0 +1,20 @@
+
+
+
+document.write("BREAK STATEMENT <br>");
+
+for ( var i = 0 ; i < 10; i++){
+    if(i == 4){
+        break;
+    }
+    document.write(i + "<br>");
+}
+
+document.write("CONTINUE  STATEMENT <br>");
+
+for ( var i = 0 ; i < 10; i++){
+    if(i == 4){
+        continue;
+    }
+    document.write(i + "<br>");
+}
