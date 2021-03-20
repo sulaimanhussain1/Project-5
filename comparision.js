44504f4f1ad5fce609a227b4c4660ecbd56ae53b
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-03-20 08:56:23 +0500

    This is practice file of javascript

---
+++ comparision.js
@@ -0,0 +1,21 @@
+
+
+
+// Comparision Operator 
+
+var a = 23;
+var b = 21;
+
+document.write( a > b );   // true
+document.write("<br>");    
+document.write( a < b );   // false 
+document.write("<br>");
+document.write( a == b);   // false
+document.write("<br>"); 
+document.write( a === b ); // false
+document.write("<br>"); 
+document.write( a >= b);   // true
+document.write("<br>"); 
+document.write( a <= b );  // false
+document.write("<br>");
+document.write( a != b);   //true
---
+++ task2.js
@@ -0,0 +1,43 @@
+// var nam = "zia";
+// function hello(){
+//     var nam = "khan";
+//     console.log(nam);
+// }
+// hello()
+// console.log(nam);
+
+// var a = "6";
+// var b = "3";
+// alert(a - b);
+
+// var city = ["karachi" , "punjab" , "Quetta "];
+// alert(city[3]);
+
+// var a1 = 16;
+// var a3 = 4;
+// var a2 = "1";
+// alert(a1 + a2 + a3);
+// var fruit = ["apple" , "banana" , "grapse" , "pineapple "];
+// fruit.splice(2 , 0 , "peach" , "cherry");
+// alert(fruit);
+
+// alert(Math.round(15.47));
+
+// var a = 5;
+// a = ++a + ++a;
+// alert(a);
+
+// for (var i=0; i<5 ; i++ ){
+//     console.log(i);
+// }
+// alert(i);
+
+// alert("1.55".toNumber());
+
+// for (var i=1; i<=10; i++){
+//     console.log("hello world");
+// }
+
+
+function printMessage("Hello world"){alert("hello world")}
+printMessage();
