7fa92978f443f0228bf3d797a5183efcf0d30b69
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-04-18 03:33:27 +0500

    This is practice file

---
+++ loop.js
@@ -0,0 +1,12 @@
+
+
+
+//  print table ..
+
+var num =parseInt(prompt("Enter table number  "));
+document.write("<h1> TABLE </h1>" );
+
+for (var i = 1; i <= 10; i++){
+    document.write(num + " x " + i + " = " + ( num * i ));
+    document.write("<br>");
+} 
