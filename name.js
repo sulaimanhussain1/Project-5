754e2328547e0e4f0c572830e0ab5788158d2cb9
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-05-08 20:19:57 +0500

    This is practice file.

---
+++ name.js
@@ -0,0 +1,20 @@
+
+
+var a = "legal and illegal name.";
+var a1 = 20;
+var a_3 = 30;
+var $a = 40;
+var a$ = 50;
+var 2a = 60;  //  this is the error. variable name can't be start be numbers.
+
+console.log(a1);
+console.log(a);
+
+
+// javascript is a case sensitive ..
+
+var rose = "hello" ; 
+var Rose = "Hello" ;
+
+console.log(rose);
+console.log(ROSE);     // this is the case sensitive ... error ....
